be2b8b5214e055ed55fbbd1b3167c694dc8e8b69
kirillkovalenko1 <144131670+kirillkovalenko1@users.noreply.github.com>
2023-10-02 11:44:32 +0400

    Delete src directory

--- src/main/java/MainPage.java
+++
@@ -1,5 +0,0 @@
-public class MainPage {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/constants/MainUrl.java
+++
@@ -1,5 +0,0 @@
-package constants;
-
-public class MainUrl {
-    public static final String MAIN_PAGE_URL = "https://qa-scooter.praktikum-services.ru/";
-}
